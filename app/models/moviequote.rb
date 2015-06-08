f317ee90f9c4ae5d02f55743c4e14b9880cbef18
tuskerette <valeria.graffeo@gmail.com>
2015-06-07 23:07:30 -0400

    edited order of users in model

--- app/models/moviequote.rb
+++ app/models/moviequote.rb
@@ -1,5 +1,4 @@
 class Moviequote < ActiveRecord::Base
-
   has_many :guesses, dependent: :destroy
-
+  # Moviequote.all.shuffle
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :guesses, dependent: :destroy
   has_many :moviequotes, through: :guesses
+  User.order('name')
 end
